fc42fbc451a7504753d18300678972c49c78deb6
Jeff Khuu <heyjeffkhuu@gmail.com>
2025-07-01 21:39:29 -0600

    Allow cross-origin requests

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ import logging
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_restful import Api
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+CORS(app)
 api = Api(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
